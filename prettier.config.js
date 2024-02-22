35f58d16a24727dbc77d4e3dbf44543ab7e97e7d
Samuel Lee <lee.sam1492@gmail.com>
2024-02-22 04:22:03 -0700

    - installed/removed dependencies - Changed postcss extension to js - Added properties to prettier config - Added content paths to tailwind config -

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,14 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    "@ianvs/prettier-plugin-sort-imports",
+  ],
+  semi: false,
+  doubleQuote: true,
+  trailingComma: "all",
+  pluginSearchDirs: false,
+  importOrder: ["^@", "^[a-zA-Z0-9-]+", "^[./]"],
 };
 
 export default config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,12 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
